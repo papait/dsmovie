448566d6c6d3d72acdbf584036ad3b48f8ee201e
papait <leonardovareschi@gmail.com>
2022-01-10 17:30:47 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,26 @@
-import Navbar from "./components/Navbar";
-
+import {
+  // Componentes importados do Router Dom
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+// componentes do react que eu criei
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+import Navbar from "components/Navbar";
 
 function App() {
   return (
-    
-    <Navbar/>
+    // Inicia configuração das rotas e Navbar inicia todas paginas
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,8 @@
+function Form () {
+    return (
+        <h1>Pagina de form</h1>
+    );
+
+}
+
+export default Form;
